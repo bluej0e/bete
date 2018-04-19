78e4f0f72d7f3e861fb8f4fadd17c0c3c972aae6
Rodrigo <rv.rodrigo.viola@gmail.com>
2018-04-19 17:08:13 -0300

    removed footer jnk
    a

--- wp-content/themes/ZEN 2.0/footer-prueba1.php
+++ wp-content/themes/ZEN 2.0/footer-prueba1.php
@@ -31,7 +31,7 @@ a.side_help:hover{
 </style>
 
 <div class="row">
-	<div class="four columns">
+<!-- 	<div class="four columns">
 		<h3>Follow us on Facebook</h3>
 		<br>
 		<div class="fb-page" data-href="https://www.facebook.com/betesportcom/" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" hide_cta="true" data-show-facepile="true">
@@ -41,24 +41,24 @@ a.side_help:hover{
 				</blockquote>
 			</div>
 		</div>
-	</div>
-	<div class="four columns">
+	</div> -->
+	<div class="twelve columns">
 		<h3>New to eSports betting?</h3>
 		<br>
-		<a class="tournaments" style="width:100%;clear:both;" href="http://www.bet-esport.com/so-youre-looking-to-bet-money-on-esports/" title="WHAT ARE ESPORTS?">WHAT ARE ESPORTS?</a>
-		<a class="tournaments" style="width:100%;clear:both;" href="http://www.bet-esport.com/how-to-bet-on-esports/" title="HOW TO BET ON ESPORTS">HOW TO BET ON ESPORTS</a>
-		<a class="tournaments" style="width:100%;clear:both;" href="http://www.bet-esport.com/esports-betting-houses/" title="ESPORTS BETTING HOUSES">ESPORTS BETTING HOUSES</a>
-		<a class="tournaments" style="width:100%;clear:both;" href="http://www.bet-esport.com/esports-betting-tips/" title="ESPORTS BETTING TIPS">ESPORTS BETTING TIPS</a>
+		<a class="tournaments" href="http://www.bet-esport.com/so-youre-looking-to-bet-money-on-esports/" title="WHAT ARE ESPORTS?">WHAT ARE ESPORTS?</a>
+		<a class="tournaments" href="http://www.bet-esport.com/how-to-bet-on-esports/" title="HOW TO BET ON ESPORTS">HOW TO BET ON ESPORTS</a>
+		<a class="tournaments" href="http://www.bet-esport.com/esports-betting-houses/" title="ESPORTS BETTING HOUSES">ESPORTS BETTING HOUSES</a>
+		<a class="tournaments" href="http://www.bet-esport.com/esports-betting-tips/" title="ESPORTS BETTING TIPS">ESPORTS BETTING TIPS</a>
 		<br>
 	</div>
-	<div class="four columns">
+<!-- 	<div class="four columns">
 		<h4>Check out this video guide to understand how odds work</h4>
 		<br>
 		<div class="flex-video" style="height:150px!important; padding-bottom: 150px;">
 			<iframe src="https://www.youtube.com/embed/seGEKQdzyYY?rel=0" frameborder="0" allowfullscreen="allowfullscreen">
 			</iframe>
 		</div>
-	</div>
+	</div> -->
 <div style="width:100%;clear:both;"></div>
 	<div class="footer">
 		<div class="twelve columns" >
