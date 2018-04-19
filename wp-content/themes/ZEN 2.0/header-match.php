62a747c9515e038a7e52bd095084111b5f8bc1e9
Rodrigo <rv.rodrigo.viola@gmail.com>
2018-04-19 17:37:16 -0300

    jquery 3
    a

--- wp-content/themes/ZEN 2.0/header-match.php
+++ wp-content/themes/ZEN 2.0/header-match.php
@@ -19,7 +19,7 @@
 	<meta name="googlebot" content="<?php echo $robocop; ?>" />
 	<meta name="revisit-after" content="2 days" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="/js/jquery.simple.timer.js"></script>
 <!--<link rel="stylesheet" type="text/css" href="/s2.css">
 	<link rel="stylesheet" type="text/css" href="/s2m.css">-->
