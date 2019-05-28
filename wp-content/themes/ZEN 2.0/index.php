0d9b17f71446bf9e4fc886a2bbd27945726be41e
James Shin <jamesphalanx@gmail.com>
2019-05-28 03:28:11 -0400

    qqqqq

--- wp-content/themes/ZEN 2.0/index.php
+++ wp-content/themes/ZEN 2.0/index.php
@@ -1,14 +1,10 @@
 <?php get_header('prueba1');?>
 <div class="row">
   <div class="twelve columns">
-    <!--  <br>
+     <br>
    <h1>Welcome to Bet eSport!</h1>
-    <h5>Everything you need to know about betting on eSports all in one place. Bet eSport combines information about matches, teams, tournaments, and gives you a look into the world of eSports betting. Here you’ll find live odds, articles, and information to help you win through eSports bets.
-      <br> 
-      <br>           
-      New to <a href="https://www.bet-esport.com/so-youre-looking-to-bet-money-on-esports/"><strong>eSports betting</strong></a>? Check out the links on the right
-    </h5>
-    <hr> -->
+
+    <hr>
 
 <!-- < ?php include('buttonsbar.php');?> -->
 
