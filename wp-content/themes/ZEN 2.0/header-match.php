9a2094267a7add301f1777d851db91ee97a9d530
James Shin <jamesphalanx@gmail.com>
2019-05-28 14:33:11 -0400

    favicon

--- wp-content/themes/ZEN 2.0/header-match.php
+++ wp-content/themes/ZEN 2.0/header-match.php
@@ -22,7 +22,7 @@
 	<script src="/js/jquery.simple.timer.js"></script>
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="/font-awesome.css">
-	<link rel="icon" href="/i/favicon.png" type="image/png" />
+	<link rel="icon" href="/i/favicon.ico" />
 	<link rel="stylesheet" type="text/css" href="/s2.css">
 	<link rel="stylesheet" type="text/css" href="/s2m.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
